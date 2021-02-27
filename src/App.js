d733d6bfa5617f0392cdf149c2cbdd71ff61962f
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-02-27 22:05:48 +0900

    create functional component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
+// class App extends Component {
+//   render() {
 
-    // div tag の部分が jsx(JavaScript XML)
-    // React を用いて HTML を出力するための言語
-    // 直感的に HTML を表現できるのが、JSX を使う利点
-    // return <div><h1>Hello, world</h1></div>
+//     // div tag の部分が jsx(JavaScript XML)
+//     // React を用いて HTML を出力するための言語
+//     // 直感的に HTML を表現できるのが、JSX を使う利点
+//     // return <div><h1>Hello, world</h1></div>
 
-    // JavaScript を用いて同じものを書く
-    // return React.createElement(
-    //   "div",
-    //   null,
-    //   "Hello, world"
-    // );
+//     // JavaScript を用いて同じものを書く
+//     // return React.createElement(
+//     //   "div",
+//     //   null,
+//     //   "Hello, world"
+//     // );
 
-    // const greeting = "Hi";
+//     // const greeting = "Hi";
 
-    // JSX の中で JS を記述するときは {}
-    // JS の予約語とかぶらないように className が使われる
-    // const dom = <h1 className="greeting">{greeting}</h1>
+//     // JSX の中で JS を記述するときは {}
+//     // JS の予約語とかぶらないように className が使われる
+//     // const dom = <h1 className="greeting">{greeting}</h1>
 
-    // const dom = <h1>Hi!</h1>
-    // return dom;
+//     // const dom = <h1>Hi!</h1>
+//     // return dom;
 
-    // return <input type="text" onClick={() => { console.log("Clicked.")}} />
-    // return <input type="text" onChange={() => { console.log("Clicked.")}} />
-    return (
-      // JSX は一つのタグを返さないといけないので、 div を使用
-      // でも div を rendering したくない場合は、 React.Fragment を使用
-      // <div>
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => { console.log("Clicked")}}/>
-      </React.Fragment>
-      // </div>
-    )
-    }
+//     // return <input type="text" onClick={() => { console.log("Clicked.")}} />
+//     // return <input type="text" onChange={() => { console.log("Clicked.")}} />
+//     return (
+//       // JSX は一つのタグを返さないといけないので、 div を使用
+//       // でも div を rendering したくない場合は、 React.Fragment を使用
+//       // <div>
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => { console.log("Clicked")}}/>
+//       </React.Fragment>
+//       // </div>
+//     )
+//     }
+// }
+
+// functional component
+const App = () => {
+  return(
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  ) 
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;

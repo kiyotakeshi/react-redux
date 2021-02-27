f11a8d8208063f104939e8fee02edb54844be87b
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-02-27 10:51:19 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     // div tag の部分が jsx(JavaScript XML)
     // React を用いて HTML を出力するための言語
     // 直感的に HTML を表現できるのが、JSX を使う利点
-    return <h1>Hello, world</h1>
+    // return <div><h1>Hello, world</h1></div>
 
     // JavaScript を用いて同じものを書く
     // return React.createElement(
@@ -14,7 +14,29 @@ class App extends Component {
     //   null,
     //   "Hello, world"
     // );
-  }
+
+    // const greeting = "Hi";
+
+    // JSX の中で JS を記述するときは {}
+    // JS の予約語とかぶらないように className が使われる
+    // const dom = <h1 className="greeting">{greeting}</h1>
+
+    // const dom = <h1>Hi!</h1>
+    // return dom;
+
+    // return <input type="text" onClick={() => { console.log("Clicked.")}} />
+    // return <input type="text" onChange={() => { console.log("Clicked.")}} />
+    return (
+      // JSX は一つのタグを返さないといけないので、 div を使用
+      // でも div を rendering したくない場合は、 React.Fragment を使用
+      // <div>
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => { console.log("Clicked")}}/>
+      </React.Fragment>
+      // </div>
+    )
+    }
 }
 
 export default App;
